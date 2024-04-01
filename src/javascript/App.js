00dad96b85b4301973799d4ad0a1af0141b1fe21
YahyaBinUsman <yahyabinusman7@gmail.com>
2024-04-01 16:55:38 +0500

    Fix GitHub Pages deployment

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,6 +1,7 @@
 /* Updated App.js */
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, NavLink } from 'react-router-dom';
+import {  Route, Routes, Link, NavLink } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
 import Home from './Home';
