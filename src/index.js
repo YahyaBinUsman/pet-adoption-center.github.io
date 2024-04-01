76a855f35f1f7ca495186d2fff17f5136b7ffa00
YahyaBinUsman <yahyabinusman7@gmail.com>
2024-04-01 19:07:59 +0500

    Update React Router configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';  
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './javascript/App';
 import reportWebVitals from './reportWebVitals';
 import './css/body.css';
@@ -9,11 +8,9 @@ import './css/body.css';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
-  <BrowserRouter basename="/pet-adoption-center.github.io"> {/* Add basename */}
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
 reportWebVitals();
