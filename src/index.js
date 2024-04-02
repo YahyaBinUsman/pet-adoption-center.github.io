b1a41fb7c4989c43f2f279f802676ff247cfe1c7
YahyaBinUsman <yahyabinusman7@gmail.com>
2024-04-03 00:21:58 +0500

    Image Showing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-// index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';  
 import App from './javascript/App';
 import reportWebVitals from './reportWebVitals';
 import './css/body.css';
 
-
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+
+root.render(
+  <React.StrictMode>
+    <Router basename="/pet-adoption-center.github.io">
+      <App />
+    </Router>
+  </React.StrictMode>
+);
 
 reportWebVitals();
