bc879500c71ada11b8b0323b899a8875e14c0fc4
YahyaBinUsman <yahyabinusman7@gmail.com>
2024-04-03 00:44:42 +0500

    Reload Issue fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
+// index.js
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Add this line
 import { createRoot } from 'react-dom/client';  
 import App from './javascript/App';
 import reportWebVitals from './reportWebVitals';
 import './css/body.css';
 
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <Router basename="/pet-adoption-center.github.io">
-      <App />
-    </Router>
-  </React.StrictMode>
-);
+root.render(<React.StrictMode><App /></React.StrictMode>);
 
 reportWebVitals();
